1c150f192d4b38e7e5902de8672ab2a5c1f7a6f3
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2017-08-16 17:06:12 +0200

    modifs home

---
+++ index_steph.php
@@ -0,0 +1,181 @@
+﻿
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+<html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>Thiga - Growth Agency</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+        <?php include('head-global.php'); ?>
+
+    </head>
+
+    <body>
+    	<?php include('gtm-body.php'); ?>
+		<?php include('navigation.php'); ?>
+		<div class="main-container">
+			<header class="page-header">
+				<div class="background-image-holder parallax-background">
+					<img class="background-image" alt="Background Image" src="/img/wallform.png">
+				</div>
+				
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 text-center">
+							<img alt="logo" class="logo" src="/img/growth-min-white.png">
+							<h1 class="text-white space-bottom-medium">Nous sommes des Product Managers orientés Growth</h1>
+						</div>
+					</div>
+				</div>
+			</header>
+			
+			<section class="pure-text-centered">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 text-center">
+							<span class="sub alt-font">THIGA - Growth Agency</span>
+							<h1><strong>Nous vous aidons à construire, développer et faire croître leurs produits numériques.</strong></h1>
+							<p class="lead">Nous accompagnons les entreprises à accélérer leur croissance en optimisant leur produit numérique. Notre expertise est au croisement de la data et de la connaissance utilisateur, du product management et du marketing.
+							</p>
+						</div>
+					</div>
+		
+				</div>
+			</section>
+			<section class="duplicatable-content">
+			
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 text-center">
+							<h1>Nos offres</h1>
+						</div>
+					</div>
+					<p>Vous cherchez à développer votre croissance ? Nous vous accompagnons dans l’optimisation de vos produits numériques, la mise en place d’une organisation “data informed”, l’organisation de vos équipes pour gagner en agilité et performance,la qualification et la croissance de votre audience.<br>
+					<br>
+					
+					</p>
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="feature feature-icon-large">
+								<div class="pull-left">
+									<a href="offre-idee-mvp.php"><i class="icon icon-genius"></i></a>
+								</div>
+								<div class="pull-right">
+									<h5><a href="offre-idee-mvp.php">Growth Strategy & Growth Marketing</a></h5>
+									<p>
+										Nous analysons votre Product Market Fit, votre Business Model, les parcours utilisateurs, la segmentation, les leviers d’acquisition exploités, … Nous vous aidons à optimiser les composantes de votre modèle à chacune des étapes de votre développement et à identifier les meilleurs moteurs de croissance.
+ 
+									</p>
+								</div>
+							</div>
+						</div>
+						
+						<div class="col-sm-6">
+							<div class="feature feature-icon-large">
+								<div class="pull-left">
+									<<a href="offre-optimisation-produit.php"><i class="icon icon-linegraph"></i></a>
+								</div>
+								<div class="pull-right">
+									<h5><a href="offre-optimisation-produit.php">Optimisation de la conversion</a></h5>
+									<p>
+										Nous analysons les tunnels de conversion (AARRR), les parcours utilisateurs, les données, les insights … afin d’identifier des axes d’amélioration à investiguer (testing). Nous vous aidons à mettre en place une démarche orientée croissance et à coordonner vos équipes techniques et marketing pour vous aider à gagner en vélocité et en performances.
+
+									</p>
+								</div>
+							</div>
+						</div>
+						
+						<div class="col-sm-6">
+							<div class="feature feature-icon-large">
+								<div class="pull-left">
+									<a href="offre-organisation-produit.php"><i class="icon icon-tools-2"></i></a>
+								</div>
+								<div class="pull-right">
+									<h5><a href="offre-organisation-produit.php">Data, Analytics & Connaissance utilisateurs</a></h5>
+									<p>
+										Nous vous aidons à sélectionner et à installer les bons outils, à optimiser de façon itérative le tracking en place, à segmenter vos utilisateurs, à connaître les usages de vos produits numériques, à suivre les bons indicateurs… afin de vous éclairer dans les ajustements nécessaires et vos prises de décisions futures.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			
+			</section>
+			
+			<section id="clients" class="clients">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 text-center">
+							<p class="lead">Nous les avons accompagné sur la création de produits numériques novateurs et sur la mise en place d'organisations orientées produit<br></p>
+						</div>
+					</div>
+					
+					<div class="row client-row">
+						<div class="row-wrapper">
+							<div class="col-sm-3 text-center">
+								<img alt="client" src="/img/logo-vsc-fb-2-min.jpg">
+							</div>
+						
+							<div class="col-sm-3 text-center">
+								<img alt="client" src="/img/sarenza-logo-1024x253-min.png">
+							</div>
+						
+							<div class="col-sm-3 text-center">
+								<img alt="client" src="/img/Deezer_logo-min.png">
+							</div>
+						
+							<div class="col-sm-3 text-center">
+								<img alt="client" src="/img/coyote-min.jpg">
+							</div>
+						</div>
+					</div>
+					
+					<div class="row client-row">
+						<div class="row-wrapper">
+							<div class="col-sm-3 text-center">
+								<img alt="client" src="/img/mediapart-min.png">
+							</div>
+						
+							<div class="col-sm-3 text-center">
+								<img alt="client" src="/img/AXA_Logo.svg-min.png">
+							</div>
+						
+							<div class="col-sm-3 text-center">
+								<img alt="client" src="/img/1024px-BNP_Paribas.svg_-min.png">
+							</div>
+						
+							<div class="col-sm-3 text-center">
+								<img alt="client" src="/img/640px-Decathlon_Logo-min.png">
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			
+			<section class="strip bg-secondary-1">
+				<div class="container">
+					<div class="row clearfix">
+						<div class="col-sm-6 col-xs-12 pull-left">
+							<h3 class="text-white"><span><strong>Envie de vous former au Product Management, au Design Thinking, aux Design Sprints ?</span></h3>
+						</div>
+						
+						<div class="col-sm-4 col-xs-12 pull-right text-right">
+							<a href="http://academy.thiga.fr/formations" target="_blank" class="btn btn-primary btn-white">Voir les formations Thiga Academy<br></a>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			
+		</div>
+		
+<?php include('footer.php'); ?>
+
+</html>		
