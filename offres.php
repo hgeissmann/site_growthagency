af5e0d493a89de83f88418be851e2b5fcd4ab0e5
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2017-08-25 11:41:35 +0200

    ajout de la page offre

---
+++ offres.php
@@ -0,0 +1,298 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+<html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>Thiga - Growth Agency - Offres</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+        <?php include('head-global.php'); ?>
+
+    </head>
+
+    <body>
+    	<?php include('gtm-body.php'); ?>
+		<?php include('navigation.php'); ?>
+		<div class="main-container">
+			<header class="page-header" style="padding-top: 209px;">
+                <div class="background-image-holder parallax-background skrollable skrollable-between" data-bottom-top="-webkit-transform: translate3d(0px,-100px, 0px)" data-center="-webkit-transform: translate3d(0px,0px, 0px)" data-top-bottom="-webkit-transform: translate3d(0px,100px, 0px)" style="background: url(&quot;img/hero6.jpg&quot;) 50% 0%; transform: translate3d(0px, 33.6397px, 0px);">
+                    <img class="background-image" alt="Background Image" src="img/hero6.jpg" style="display: none;">
+                </div>
+                
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <span class="text-white alt-font">OFFRES</span>
+                            <h1 class="text-white">Nos Offres Growth</h1>
+                            <p class="text-white lead"><i>Nous aidons les entreprises à accélérer leur croissance
+en optimisant leur produit numérique</i></p>
+                        </div>
+                    </div>
+                </div>
+            </header>
+			
+            
+            <section class="text-banner" style="padding-bottom:50px;">
+				<div class="container text-center">
+					<div class="row">
+						<div class="col-sm-12" >
+							<ul class="icons-large" >
+								<li><i class="icon icon-desktop"></i></li>
+								<li><i class="icon icon-tools-2"></i></li>
+								<li><i class="icon icon-compass"></i></li>
+							</ul>
+							<h1 class="center-block">Découvrez l'offre adaptée à vos besoins</h1>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-10 col-sm-offset-1">
+							<p class="lead">
+								Nos offres s'adaptent au plus près de vos besoins. eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, 
+							</p>
+						</div>
+					</div>
+					
+					</div>
+				</div>
+			</section>
+            
+			
+			<section class="side-image text-heavy clearfix">
+				<div class="image-container col-md-5 col-sm-3 pull-left">
+					<div class="background-image-holder" style="background: url(&quot;img/side2.jpg&quot;) 50% 0%;">
+						<img class="background-image" alt="Background Image" src="img/side2.jpg" style="display: none;">
+					</div>
+				</div>
+				
+				<div class="container">
+				
+					<div class="row">
+				
+						<div class="col-md-6 col-md-offset-6 col-sm-8 col-sm-offset-4 content clearfix">
+							<h1>Growth Strategy et Growth Marketing</h1>
+							<p class="lead" contenteditable="">Vous cherchez à développer votre croissance ? Nous analysons votre Product Market Fit, votre Business Model, les parcours utilisateurs, les leviers d’acquisition exploités, … Afin d'identifier les meilleurs moteurs de croissance</p>
+                        
+							<a href="#contact" class="btn btn-primary">Nous contacter</a><br>
+							<div class="col-sm-6 no-pad-left feature">
+								<h5 contenteditable>Nos consultants</h5>
+								<p contenteditable>
+									Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labor
+								</p>
+							</div>
+						
+							<div class="col-sm-6 no-pad-left feature">
+								<h5 contenteditable>Mode d'accompagnement</h5>
+								<p contenteditable>
+									Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labor
+								</p>
+							</div>
+						</div>
+					
+					</div>
+				
+				</div>
+			</section>
+            
+            
+            
+            
+            
+            
+            <section class="side-image text-heavy clearfix">
+				
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6 content col-sm-8 clearfix">
+								<h1 contenteditable>Data, Analytics et Connaissance utilisateurs</h1>
+							<p contenteditable class="lead">
+								Nous vous aidons à choisir et à installer les bons outils, à optimiser le tracking, à segmenter vos clients, à connaître les usages de vos produits, à suivre les bons indicateurs… afin de vous éclairer dans vos prises de décisions futures.
+							</p>
+							<a href="#contact" class="btn btn-primary">Nous contacter</a><br>
+							<div class="col-sm-6 no-pad-left feature">
+								<h5 contenteditable>Nos consultants</h5>
+								<p contenteditable>
+									Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labor
+								</p>
+							</div>
+						
+							<div class="col-sm-6 no-pad-left feature">
+								<h5 contenteditable>Mode d'accompagnement</h5>
+								<p contenteditable>
+									Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labor
+								</p>
+							</div>	
+							
+							</div>
+					
+						
+					</div>
+				</div>
+				
+				<div class="image-container col-md-5 col-sm-3 pull-right">
+					<div class="background-image-holder" style="background: url(&quot;img/hero10.jpg&quot;) 50% 0%;">
+						<img class="background-image" alt="Background Image" src="img/hero10.jpg" style="display: none;">
+					</div>
+				</div>
+				
+			</section>
+            
+            
+            
+            
+            
+            
+            
+            <section class="side-image text-heavy clearfix">
+				<div class="image-container col-md-5 col-sm-3 pull-left">
+					<div class="background-image-holder" style="background: url(&quot;img/side2.jpg&quot;) 50% 0%;">
+						<img class="background-image" alt="Background Image" src="img/side2.jpg" style="display: none;">
+					</div>
+				</div>
+				
+				<div class="container">
+				
+					<div class="row">
+				
+						<div class="col-md-6 col-md-offset-6 col-sm-8 col-sm-offset-4 content clearfix">
+							<h1 contenteditable>Optimisation de la conversion</h1>
+							<p contenteditable class="lead">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+							</p>
+							<a href="#contact" class="btn btn-primary">Nous contacter</a><br>
+							<div class="col-sm-6 no-pad-left feature">
+								<h5 contenteditable>Nos consultants</h5>
+								<p contenteditable>
+									Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labor
+								</p>
+							</div>
+						
+							<div class="col-sm-6 no-pad-left feature">
+								<h5 contenteditable>Mode d'accompagnement</h5>
+								<p contenteditable>
+									Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labor
+								</p>
+							</div>
+						</div>
+					
+					</div>
+				
+				</div>
+			</section>
+            
+            
+            
+            
+            
+            
+            <section class="side-image text-heavy clearfix">
+				
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6 content col-sm-8 clearfix">
+				            <h1 contenteditable>S'organiser pour croître</h1>
+							<p contenteditable class="lead">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+							</p>
+							<a href="#contact" class="btn btn-primary">Nous contacter</a><br>
+							<div class="col-sm-6 no-pad-left feature">
+								<h5 contenteditable>Nos consultants</h5>
+								<p contenteditable>
+									Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labor
+								</p>
+							</div>
+						
+							<div class="col-sm-6 no-pad-left feature">
+								<h5 contenteditable>Mode d'accompagnement</h5>
+								<p contenteditable>
+									Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labor
+								</p>
+							</div>	
+							
+							</div>
+					
+						
+					</div>
+				</div>
+				
+				<div class="image-container col-md-5 col-sm-3 pull-right">
+					<div class="background-image-holder" style="background: url(&quot;img/hero10.jpg&quot;) 50% 0%;">
+						<img class="background-image" alt="Background Image" src="img/hero10.jpg" style="display: none;">
+					</div>
+				</div>
+				
+			</section>
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            <script>                        
+                var postDataToWebhook = function(){    
+                  //url to your webhook
+                  var webHookUrl="https://hooks.slack.com/services/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"; 
+                  var contact = $('.form-email').val(); 
+                  var message = $('.form-message').val();
+
+                  if(!contact){$('.form-error').show();return;}
+
+                  //https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+                  var oReq = new XMLHttpRequest();
+                  var payload={text:'@le_couss @nicogalland',attachments: [
+                        {title:contact, text:message||'pas de message'}]};
+
+                //register method called after data has been sent method is executed
+                  oReq.open("POST", webHookUrl,true);
+                  oReq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                  oReq.send(JSON.stringify(payload));
+                  $('.form-success').show();
+                    $('.form-error').hide();
+                };
+        </script>            
+            <section id="contact" class="contact-center">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 text-center">
+							<h1>Contact<br></h1>
+							<p contenteditable class="lead">
+								Vous êtes intéressé(e) par nos offres ? Vous souhaitez obtenir plus d’informations sur nos métiers ? Vous avez envie de nous rejoindre ?<br><br>
+								Laissez nous un mail ou un numéro de téléphone et nous vous recontacterons très rapidement !</p>
+						</div>
+					</div>
+					
+					<div class="row">
+						<div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+							<div class="form-wrapper clearfix">
+								<form class="form-contact email-form">
+									<div class="inputs-wrapper">										
+										<input class="form-email validate-required" type="text" name="email" placeholder="Votre email ou numéro de téléphone">
+										<textarea class="form-message validate-required" name="message" placeholder="Votre message (optionnel)"></textarea>
+									</div>
+									<button type="button" class="btn btn-primary" onclick="postDataToWebhook()" value="Send Form">Contactez-moi !</button>
+									<div class="form-success">
+										<span class="text-white">Merci, à tout de suite !</span>
+									</div>
+									<div class="form-error">
+										<span class="text-white">Merci de remplir le champ "mail ou téléphone" :)</span>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+            
+		</div>
+		
+		<?php include('footer.php'); ?>
+</html>
+				
